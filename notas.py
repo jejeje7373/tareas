e7a2849988fd067c9b266ab30ea86f7ecd32c917
jejeje <aymcubillosss@gmail.com>
2025-07-01 16:20:51 -0400

    Add files via upload

---
+++ notas.py
@@ -0,0 +1,11 @@
+#ingrese las  notas 
+n1=float(input("ingrese nota 1: "))
+n2=float(input("ingrese nota 2: "))
+n3=float(input("ingrese nota 3: "))
+promedio=n1*0.3+n2*0.4+n3*0.3
+promedio=round(promedio, 1)
+print("el promedio de presentacion final es: ",promedio)
+nfinal=float(input("ingrese la nota final: "))
+promedio_final=promedio*0.6+nfinal*0.4
+promedio_final=round(promedio_final, 1)
+print("El promedio Final es : ",promedio_final)
